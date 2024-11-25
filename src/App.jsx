c25273e7403d7bcaf1e087aeb2664fb62f9bf543
Eriza anjelika vega <139231295+ErizaAnjelika@users.noreply.github.com>
2024-11-26 05:04:48 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import ReactGA from "react-ga";
+import $ from "jquery";
+import "./App.css";
+import Header from "./components/Header";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      foo: "bar",
+      resumeData: {},
+    };
+    ReactGA.initialize("UA-110570651-1");
+    ReactGA.pageview(window.location.pathname);
+  }
+
+  getResumeData() {
+    $.ajax({
+      url: "/resumeData.json",
+      dataType: "json",
+      cache: false,
+      success: function (data) {
+        this.setState({ resumeData: data });
+      }.bind(this),
+      error: function (xhr, status, err) {
+        alert(err);
+        console.log(err);
+      },
+    });
+  }
+
+  componentDidMount() {
+    this.getResumeData();
+  }
+  render() {
+    return (
+      <div className="App">
+        <Header data={this.state.resumeData.main} />
+      </div>
+    );
+  }
+}
